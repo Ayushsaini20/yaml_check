b39d82a3a44ae53ef24cd462e9a77b51217c734d
AyushSaini09 <146984591+AyushSaini09@users.noreply.github.com>
2025-05-05 14:55:06 +0530

    Create even_odd.py

---
+++ even_odd.py
@@ -0,0 +1,8 @@
+# Ask the user for a number
+num = int(input("Enter a number: "))
+
+# Check if it's even or odd
+if num % 2 == 0:
+    print(f"{num} is Even ✅")
+else:
+    print(f"{num} is Odd 🔢")

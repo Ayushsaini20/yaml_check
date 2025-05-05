8b2bbce144b018bdf5b059f6a5ff79cfcaa21996
AyushSaini09 <146984591+AyushSaini09@users.noreply.github.com>
2025-05-05 16:08:16 +0530

    Create factorial.go

---
+++ factorial.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "fmt"
+)
+
+func factorial(n int) int {
+    if n == 0 {
+        return 1
+    }
+    return n * factorial(n-1)
+}
+
+func main() {
+    var num int
+    fmt.Print("Enter a number: ")
+    fmt.Scanln(&num)
+
+    if num < 0 {
+        fmt.Println("Factorial is not defined for negative numbers ")
+    } else {
+        result := factorial(num)
+        fmt.Printf("Factorial of %d is %d \n", num, result)
+    }
+}

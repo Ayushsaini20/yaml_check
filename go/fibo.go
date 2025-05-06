b98bab0ec223b55bf121b01644ef3b8ed406a29b
AyushSaini09 <146984591+AyushSaini09@users.noreply.github.com>
2025-05-06 14:26:36 +0530

    Update fibo.go

--- go/fibo.go
+++ go/fibo.go
@@ -1,31 +1,31 @@
 package main
-
+ 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 )
-
+ 
 func fibonacci(n int) {
-    a, b := 0, 1
-    for i := 0; i < n; i++ {
-        fmt.Printf("%d ", a)
-        a, b = b, a+b
-    }
-    fmt.Println()
+	a, b := 0, 1
+	for i := 0; i < n; i++ {
+		fmt.Printf("%d ", a)
+		a, b = b, a+b
+	}
+	fmt.Println()
 }
-
+ 
 func main() {
-    var terms int
-    fmt.Print("Enter the number of terms: ")
-    if _, err := fmt.Scanln(&terms); err != nil {
-        fmt.Println("Error reading input:", err)
-        os.Exit(1)
-    }
-
-    if terms <= 0 {
-        fmt.Println("Please enter a positive integer.")
-    } else {
-        fmt.Printf("Fibonacci sequence with %d terms:\n", terms)
-        fibonacci(terms)
-    }
+	var terms int
+	fmt.Print("Enter the number of terms: ")
+	if _, err := fmt.Scanln(&terms); err != nil {
+		fmt.Println("Error reading input:", err)
+		os.Exit(1)
+	}
+ 
+	if terms <= 0 {
+		fmt.Println("Please enter a positive integer.")
+	} else {
+		fmt.Printf("Fibonacci sequence with %d terms:\n", terms)
+		fibonacci(terms)
+	}
 }

b8ae7e7805cbaf80e99bbe91fa8bba46bfcb65d1
AyushSaini09 <146984591+AyushSaini09@users.noreply.github.com>
2025-05-06 15:07:00 +0530

    Create envtoken.go

---
+++ go/envtoken.go
@@ -0,0 +1,46 @@
+package main
+ 
+import (
+    "encoding/json"
+    "fmt"
+    "net/http"
+    "os"
+)
+ 
+func main() {
+    token := os.Getenv("GITHUB_TOKEN")
+    if token == "" {
+        fmt.Println("GITHUB_TOKEN environment variable is not set")
+        os.Exit(1)
+    }
+ 
+    req, err := http.NewRequest("GET", "https://api.github.com/user", nil)
+    if err != nil {
+        fmt.Println("Request creation failed:", err)
+        os.Exit(1)
+    }
+ 
+    req.Header.Set("Authorization", "Bearer "+token)
+    req.Header.Set("Accept", "application/vnd.github+json")
+ 
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        fmt.Println("Request failed:", err)
+        os.Exit(1)
+    }
+    defer resp.Body.Close()
+ 
+    if resp.StatusCode != http.StatusOK {
+        fmt.Printf("GitHub API request failed with status: %s\n", resp.Status)
+        os.Exit(1)
+    }
+ 
+    var result map[string]interface{}
+    if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+        fmt.Println("Failed to decode JSON:", err)
+        os.Exit(1)
+    }
+ 
+    fmt.Println("GitHub user login:", result["login"])
+}

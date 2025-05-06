0e54334a2f7ee080cd86f6e631d9fc0eb0b8c46e
AyushSaini09 <146984591+AyushSaini09@users.noreply.github.com>
2025-05-06 17:33:15 +0530

    Create file1.go

---
+++ go/file1.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/google/go-github/v58/github"
+	"golang.org/x/oauth2"
+)
+
+func main() {
+	
+	token := "ghp_1234567890abcdefEXAMPLETOKEN"
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	// Example API call
+	repo, _, err := client.Repositories.Get(ctx, "octocat", "Hello-World")
+	if err != nil {
+		log.Fatalf("Error fetching repo: %v", err)
+	}
+
+	fmt.Printf("Repository name: %s\n", *repo.Name)
+}

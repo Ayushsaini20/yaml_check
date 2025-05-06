658de27a61930b89816756aa9e3eda00c56a7ad4
AyushSaini09 <146984591+AyushSaini09@users.noreply.github.com>
2025-05-06 14:15:36 +0530

    Update fib.go

--- go/fib.go
+++ go/fib.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "fmt"
+    "os"
 )
 
 func fibonacci(n int) {
@@ -16,12 +17,13 @@ func fibonacci(n int) {
 func main() {
     var terms int
     fmt.Print("Enter the number of terms: ")
-    fmt.Scanln(&terms)
+    if _, err := fmt.Scanln(&terms); err != nil {
+        fmt.Println("Error reading input:", err)
+        os.Exit(1)
+    }
 
     if terms <= 0 {
         fmt.Println("Please enter a positive integer.")
     } else {
         fmt.Printf("Fibonacci sequence with %d terms:\n", terms)
         fibonacci(terms)
-    }
-}
